b9014c55571618e768a415912c9cf23092a629f7
Daril Alanis <a.alanis1108@gmail.com>
2017-02-28 22:21:09 +0000

    tiny turle

--- script.js
+++ script.js
@@ -13,6 +13,26 @@ forward(50);
 }
 
 // Type your function call below
+// square();
 
 
 stamp();
+function rectangle(length,width){
+
+    right(90);
+    forward(width);
+    right(90);
+    forward(length);
+    right(90);
+    forward(length);
+    right(90);
+    forward(length);
+  
+   
+}
+
+rectangle(100,90)
+
+
+    
+
